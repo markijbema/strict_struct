afaaf35b7ff2e67cd5b5831b821c52e7a142a25d
Mark IJbema <markijbema@gmail.com>
2014-02-18 07:09:12 +0100

    Extracted module creation out of the class creation

--- lib/strict_struct.rb
+++ lib/strict_struct.rb
@@ -19,40 +19,45 @@ module StrictStruct
     end
   end
 
-  def self.new(*attributes, &block)
-    mod = Module.new do
-      define_method :initialize do |hash={}|
-        Helper.validate_arguments(hash.keys, attributes)
-        @_strict_struct_hash = Hash[hash.to_a].freeze
-      end
+  class AttributeModule < Module
+    def initialize *attributes
+      super() do
+        define_method :initialize do |hash={}|
+          Helper.validate_arguments(hash.keys, attributes)
+          @_strict_struct_hash = Hash[hash.to_a].freeze
+        end
 
-      attributes.each do |attribute|
-        define_method(attribute) do
-          @_strict_struct_hash[attribute]
+        attributes.each do |attribute|
+          define_method(attribute) do
+            @_strict_struct_hash[attribute]
+          end
+        end
+
+        def to_h
+          to_hash
+        end
+
+        def to_hash
+          Hash[@_strict_struct_hash.to_a]
+        end
+
+        define_method :== do |other|
+          return false unless self.class == other.class
+
+          attributes.all? {|name| self.send(name) == other.send(name)}
+        end
+
+        define_method :hash do
+          @_strict_struct_hash.hash
         end
       end
-
-      def to_h
-        to_hash
-      end
-
-      def to_hash
-        Hash[@_strict_struct_hash.to_a]
-      end
-
-      define_method :== do |other|
-        return false unless self.class == other.class
-
-        attributes.all? {|name| self.send(name) == other.send(name)}
-      end
-
-      define_method :hash do
-        @_strict_struct_hash.hash
-      end
     end
-    klass = Class.new
-    klass.send :include, mod
-    klass.class_eval(&block) if block_given?
-    klass
+  end
+
+  def self.new(*attributes, &block)
+    Class.new do
+      include AttributeModule.new(*attributes)
+      class_eval(&block) if block_given?
+    end
   end
 end
